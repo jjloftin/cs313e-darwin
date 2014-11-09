4040c86e31aafe1ffeb3e34b93f8e69ef1cd0644
JJ <johnloftin99@gmail.com>
2014-11-09 03:12:01 -0600

    Fixed #4

--- Darwin.py
+++ Darwin.py
@@ -1,57 +1,91 @@
 #-----------
 #Darwin.py
+#by John Loftin
 #-----------
 
 
 class Darwin :
   '''
   This is the game board class. Holds the creatures and tells them whether or not they can move as requested.
-  Goes through the board to execute instructions of creatures. Also, this holds all of the instructions
+  Goes through the board to execute instructions of creatures. 
+  
+  World Variables
+  world - the actual game board. 2D list with empty space tokens and stores pointers to creature instances
+  
+  World Attribute Variables
+  x_max - maximum horizontal expanse of the world
+  y_max - maximum vertical exxpanse of the world
+  turns - keeps track of number of turns of the simulation has been run
+  
+  Creature Tracking Variables. Keep track of whats in front of creatures when running the game board.
+  If the creature asks for allowed information about the board state, the game board returns it to the creature.
+  Boolean valued.
+  
+  empty - is the space in front empty or a wall
+  wall - is the space in front a wall
+  enemy - is the space in front a different species
   '''
   def __init__(self,x,y):
+    '''
+    Initialize the data members and construct an empty game board.
+    '''
     self.world = []
-    self.turns = 0  
+    
+    self.x_max = x-1
+    self.y_max = y-1
+    self.turns = 0
+    
+    self.empty = None
+    self.wall = None    
+    self.enemy = None
   def add(self,c,x,y):
     '''
     Add creature to the world if space is empty
     '''
-    self.world[x,y] = c
-  def execute(self,x,y):
+    
+    return ''
+
+  def go(self,x,y):
     '''
-    Creature in spot executes its turn. If empty does nothing.
+    If empty does nothing. Otherwise, tells creature to 'go' and execute its next action.
     '''
+    
+  '''
+  Space Check Methods. These are the methods a creature can use while executing its turn to check the space in front of it.
+  '''  
+  def isEmpty(self,c):
+    '''
+    Takes a creature in and returns whether or not the space in front of the creature is empty.
+    '''
+    return '1'
+  def isWall(self,c):
+    '''
+    Takes a creature in and returns whether or not the space in front of the creature is a wall.
+    '''
+    return '1'
+  
+  def isEnemy(self,c):
+    '''
+    Takes in a creature and lets you know if the space in front of the creature contains an enemy. 
+    '''
+    return '1'
+    
   def turn(self):
     '''
     Executes one run on the game board. Each creature getting to execute once.
     '''    
-
-
-
-class Species :
-  '''
-  Species class is made to hold instructions for individual creatures.
-  '''
-  def __init__(self,s):
-    self.name = s
-    self.instructions = []
-  def addInstruction (self, s) :
-    self.instructions.append(s)
-
-class Creature :
-  '''
-  Individual creatures which sit in the game board and interact with other creatues.
-  Have a list of instructions from species, a program counter to tell them
-  where in the list they are, and they have a direction. 
-  '''
-  def __init__(self,species,direction):
-    self.species = species
-    self.direction = 0
-    self.counter = 0
+    return False
+  def __str__(self):
+    '''
+    Returns the grid in printable form
+    '''
+    s = '  '
+    return s
     
-  def go(self):
-    '''
-    Creature executes directions until accomplishing one action move
-    '''
+  '''
+  Creature - World Interaction Methods. These get called when the creature executes its turn. The game board needs to reconfigure itself to account
+  for executions of turns by the creature.
+  '''  
   def hop (self, c):
     '''
     If creature has nothing obstructing it, it hops forward one space
@@ -73,4 +107,41 @@ class Creature :
     '''
     Turn creature right
     '''
+    
+class Species :
+  '''
+  Species class is made to hold instructions for individual creatures.
+  '''
+  def __init__(self,name):
+    assert type(name) is str
+    assert name[0].isalpha()
+    
+    self.name = name.lower()
+    self.instructions = []
+  def addInstruction (self, s):
+      self.instructions.append(s)
+  
+ 
+class Creature :
+  '''
+  Individual creatures which sit in the game board and interact with other creatues.
+  Have a list of instructions from species, a program counter to tell them
+  where in the list they are, and they have a direction. 
+  
+  We also hardcode in the list of actions. This weill help us know when a creature executes its turn if it committed an action
+  species - the species of the creature
+  direction - the direction of the creature stored as a string 'north', 'south', 'west', 'east'
+  counter - stores where in the species instruction list the creature is
+  actions - stores the action commands. if one of these is executed a creature ceases to execute directions
+  '''
+  def __init__(self,species,direction):
+    self.species = species
+    self.direction = direction
+    self.counter = 0
+    self.actions = ['hop', 'infect', 'left', 'right']
+  def execute(self):
+    '''
+    Creature executes directions until executing an action move. Returns that action move to the game board. 
+    '''
+    return ''
   
--- TestDarwin.py
+++ TestDarwin.py
@@ -9,87 +9,413 @@ from Darwin import Darwin, Species, Creature
 class TestDarwin(TestCase):
 
   #Species
+  
+  #basics
   def test_species1 (self):
     s = Species('dingo')
     self.assertEqual(s.name,'dingo')
-  def test_species3 (self):  
+  def test_species2 (self):  
     s = Species('dingo')
     self.assertEqual(s.instructions,[])
-  def test_species4 (self):  
+  
+  #addInstruction
+  def test_species3 (self):  
     s = Species('dingo')
     s.addInstruction('hop')
     self.assertEqual(s.instructions,['hop'])
-  def test_species5(self):
+  def test_species4(self):
     s = Species('dingo')
     s.addInstruction('hop')
     s.addInstruction('infect')
     self.assertEqual(s.instructions,['hop','infect']) 
-  def test_species6(self):
+  def test_species5(self):
     s = Species('dingo')
     s.addInstruction('hop')
     s.addInstruction('infect')
     s.addInstruction('hop')
     self.assertEqual(s.instructions,['hop','infect','hop'])
-
+  
+  #Creature
+  
+  #basics
+  def test_creature1(self):
+    s = Species('cat')
+    s.addInstruction('hop')
+    s.addInstruction('infect')
+    c = Creature(s,'north')
+    
+    self.assertEqual(c.turns, 0)
+  def test_creature2(self):
+    s = Species('cat')
+    s.addInstruction('hop')
+    s.addInstruction('infect')
+    c = Creature(s,'north')
+    
+    self.assertEqual(c.direction, 'north')
+  
+  #execute
+  def test_creature3(self):
+    s = Species('cat')
+    s.addInstruction('hop')
+    s.addInstruction('infect')
+    c = Creature(s,'north')
+    cmd = c.execute()  
+    self.assertEqual(cmd, 'hop')
+  def test_creature4(self):
+    s = Species('cat')
+    s.addInstruction('hop')
+    s.addInstruction('infect')
+    c = Creature(s,'north')
+    cmd = c.excute()
+    
+    self.assertEqual(c.counter,1)
+  def test_creature5(self):
+    s = Species('cat')
+    s.addInstruction('hop')
+    s.addInstruction('infect')
+    c = Creature(s,'north')
+    c.execute()
+    cmd = c.excute()
+    
+    self.assertEqual(cmd, 'infect')
+  def test_creature6(self):
+    s = Species('cat')
+    s.addInstruction('hop')
+    s.addInstruction('infect')
+    c = Creature(s,'north')
+    c.excute()
+    c.execute()
+    
+    self.assertEqual(c.counter,2)  
+    
   #Darwin
   def test_darwin1(self):
     d = Darwin(2,2)
-    self.assertEqual(d.world, [['.','.'],['.','.']])
+    self.assertEqual(str(d), '  01\n0 ..\n1 ..')
   def test_darwin2(self):
     d = Darwin(1,1)
-    self.assertEqual(d.world, [['.']])
-    
+    self.assertEqual(str(d),'  0\n0 .')
+  
+  #add  
   def test_darwin3(self):  
     d = Darwin(1,3)
     s = Species('cat')
     c1 = Creature(s,'south')
     c2 = Creature(s,'south')
     c3 = Creature(s,'south')
-    d.add(c1,1,1)
-    self.assertEqual(d.world,[['c','.','.']])
+    d.add(c1,0,0)
+    self.assertEqual(str(d),'  012\n0 c . .')
   def test_darwin4(self):  
     d = Darwin(1,3)
     s = Species('cat')
     c1 = Creature(s,'south')
     c2 = Creature(s,'south')
     c3 = Creature(s,'south')
-    d.add(c1,1,1)
-    d.add(c2,1,2)
-    self.assertEqual(d.world,[['c','c','.']])
+    d.add(c1,0,0)
+    d.add(c2,0,1)
+    self.assertEqual(str(d),'  012\n0 c c .')
   def test_darwin5(self):  
     d = Darwin(1,3)
     s = Species('cat')
     c1 = Creature(s,'south')
     c2 = Creature(s,'south')
     c3 = Creature(s,'south')    
-    d.add(c1,1,1)
-    d.add(c2,1,2)
-    d.add(c2,1,3)
-    self.assertEqual(d.world,[['c','c','c']])
+    d.add(c1,0,0)
+    d.add(c2,0,1)
+    d.add(c2,0,2)
+    self.assertEqual(str(d), '  0123\n0 c c c')
     
+  #go
   def test_darwin6(self):
     d = Darwin(2,2)
     s = Species('dog')
     s.addInstruction('hop')
     c = Creature(s,'south')
-    d.add(c,2,1)
+    d.add(c,1,0)
+    d.go(1,0)
+    self.assertEqual(str(d),'  01\n0 ..\n1 d.')
   def test_darwin7(self):
     d = Darwin(2,2)
     s = Species('dog')
-    c = Creature(s,'south')
-    d.add(c,2,1)
+    c = Creature(s,'north')
+    d.add(c,1,0)
+    d.go(1,0)
+    self.assertEqual(str(d),'  01\n0 d.\n1 ..')
   def test_darwin8(self):
     d = Darwin(2,2)
     s = Species('dog')
     c = Creature(s,'south')
-    
-  def test_darwin9(self):
-    pass
-  def test_darwin10(self):
-    pass
-  def test_darwin11(self):
-    pass
-  #Creature
+    d.add(c,1,0)
+    d.go(1,1)
+    self.assertEqual(str(d),'  01\n0 ..\n1 d.')
   
+  #turn  
+  def test_darwin9(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    c = Creature(s, 'east')
+    d.add(c,0,0)
+    d.turn()
+    assert 1 == 2
+    self.assertEqual(d.turns, 1)    
+  def test_darwin10(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    c = Creature(s, 'east')
+    d.add(c,0,0)
+    d.turn()
+    self.assertEqual(str(d),'  01\n0 . c')
+  
+  #isEmpty    
+  def test_darwin11(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'east')
+    d.add(c,0,0)
+    d.go(0,0)
+    self.assertEqual(True,d.isEmpty(c))
+  def test_darwin12(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'south')
+    d.add(c,0,0)
+    d.go(0,0)
+    self.assertEqual(False,d.isEmpty(c))
+  def test_darwin13(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'west')
+    d.add(c,0,0)
+    d.go(0,0)
+    self.assertEqual(False, d.isEmpty(c))
+    
+  #isWall
+  def test_darwin14(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'west')
+    d.add(c,0,0)
+    d.go(0,0)
+    self.assertEqual(True,d.isWall(c))
+  def test_darwin15(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'south')
+    d.add(c,0,0)
+    d.go(0,0)
+    self.assertEqual(True,d.isWall(c))
+  def test_darwin16(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'east')
+    d.add(c,0,0)
+    d.go(0,0)
+    self.assertEqual(False,d.isWall(c))
+    
+  #isEnemy
+  def test_darwin17(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    c = Creature(s,'south')
+    d.add(c,0,0)
+    d.go(0,0)
+    self.assertEqual(False,d.isEnemy(c))
+  def test_darwin18(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    c = Creature(s,'east')
+    c1 = Creature(s,'east')
+    d.add(c,0,0)
+    d.add(c,0,1)
+    d.go(0,0)
+    self.assertEqual(False,d.isEnemy(c))
+  def test_darwin19(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    c = Creature(s,'east')
+    d.add(c,0,0)
+    d.go(0,0)
+    self.assertEqual(False,d.isEnemy(c))  
+  def test_darwin20(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    s1 = Species('dog')
+    s1.addInstruction('hop')
+    c = Creature(s,'east')
+    c1 = Creature(s1,'east')
+    d.add(c,0,0)
+    d.add(c,0,1)
+    d.go(0,0)
+    self.assertEqual(True,d.isEnemy(c))     
+    
+  #hop
+  def test_darwin21(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'east')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.hop(c)
+    self.assertEqual(str(d), '  01\n0 . c')
+  def test_darwin22(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'west')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.hop(c)
+    self.assertEqual(str(d), '  01\n c .')
+  def test_darwin23(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'south')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.hop(c)
+    self.assertEqual(str(d), '  01\n c .')
+    
+  #left
+  def test_darwin24(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'south')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.left(c)
+    self.assertEqual('east', c.direction)  
+  def test_darwin25(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'east')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.left(c)
+    self.assertEqual('north',c.direction)
+  def test_darwin26(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'north')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.left(c)
+    self.assertEqual('west',c.direction)
+  def test_darwin27(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'west')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.left(c)
+    self.assertEqual('south',c.direction)
+  
+  #right
+  def test_darwin28(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'south')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.right(c)
+    self.assertEqual('west', c.direction)  
+  def test_darwin29(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'west')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.right(c)
+    self.assertEqual('north',c.direction)
+  def test_darwin30(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'north')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.right(c)
+    self.assertEqual('east',c.direction)
+  def test_darwin31(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('infect')
+    c = Creature(s,'east')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.right(c)
+    self.assertEqual('south',c.direction)
+  
+  #infect
+  def test_darwin32(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    c = Creature(s,'east')
+    c1 = Creature(s,'east')
+    d.add(c,0,0)
+    d.add(c,0,1)
+    d.go(0,0)
+    
+    d.infect(c)
+    self.assertEqual(str(d), '  01\n c c')
+  def test_darwin33(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    c = Creature(s,'north')
+    d.add(c,0,0)
+    d.go(0,0)
+    
+    d.right(c)
+    d.infect(c)
+    self.assertEqual(str(d),'  01\n c .')  
+  def test_darwin34(self):
+    d = Darwin(1,2)
+    s = Species('cat')
+    s.addInstruction('hop')
+    s1 = Species('dog')
+    s1.addInstruction('hop')
+    c = Creature(s,'east')
+    c1 = Creature(s1,'east')
+    d.add(c,0,0)
+    d.add(c,0,1)
+    d.go(0,0)
+    
+    d.infect(c)
+    self.assertEqual(str(d),'  01\n c c')  
+ 
+    
+
 main()
   
